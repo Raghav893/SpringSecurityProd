ceaee0811253a732741eabe16b545a8f6c85e2e1
Raghav893 <raghavarora2309@gmail.com>
2026-07-25 18:09:24 +0530

    init

---
+++ src/main/java/com/raghav/springsecurityprod/SpringSecurityProdApplication.java
@@ -0,0 +1,13 @@
+package com.raghav.springsecurityprod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityProdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityProdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/raghav/springsecurityprod/SpringSecurityProdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raghav.springsecurityprod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityProdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
