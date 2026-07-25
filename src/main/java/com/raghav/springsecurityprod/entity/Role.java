7f5c4f26d043789729d62561cfaa4106069d8af9
Raghav893 <raghavarora2309@gmail.com>
2026-07-25 18:31:31 +0530

    basic entity

---
+++ src/main/java/com/raghav/springsecurityprod/entity/Role.java
@@ -0,0 +1,5 @@
+package com.raghav.springsecurityprod.entity;
+
+public enum Role {
+    ROLE_USER, ROLE_ADMIN, ROLE_MODERATOR
+}
---
+++ src/main/java/com/raghav/springsecurityprod/entity/User.java
@@ -0,0 +1,35 @@
+package com.raghav.springsecurityprod.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false,unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles",joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role")
+    private Set<Role> roles = new HashSet<>();
+
+    private boolean enabled = true;
+
+}
