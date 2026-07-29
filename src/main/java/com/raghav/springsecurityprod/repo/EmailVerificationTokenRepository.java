18838eaba42f0abf6c649566a014d295e9567d37
Raghav893 <raghavarora2309@gmail.com>
2026-07-29 14:06:02 +0530

    Enhance `EmailVerificationTokenRepository` with token search and invalidation methods; add custom exceptions for email verification handling

--- src/main/java/com/raghav/springsecurityprod/repo/EmailVerificationTokenRepository.java
+++ src/main/java/com/raghav/springsecurityprod/repo/EmailVerificationTokenRepository.java
@@ -1,11 +1,23 @@
 package com.raghav.springsecurityprod.repo;
 
 import com.raghav.springsecurityprod.entity.EmailVerificationToken;
+import com.raghav.springsecurityprod.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface EmailVerificationTokenRepository extends JpaRepository<EmailVerificationToken, UUID> {
+
+
+    Optional<EmailVerificationToken> findByTokenHash(String tokenHash);
+
+    @Modifying
+    @Query("UPDATE EmailVerificationToken  t set t.used = true where t.user=:user AND  t.used=false")
+    void  invalidateAllUnusedForUser(@Param("user")User user);
 }
